d92bdeb9067254f8637549644825d5717ba3f24e
Shavidu Gayashan <95381029+Shavindu-Gayashan@users.noreply.github.com>
2025-10-12 13:01:43 +0530

    feat(navigation): refactor BottomNavigation component to use icons and improve styling

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Profile from './src/screens/Profile.jsx';
 import ColorDemo from './src/screens/ColorDemo.jsx';
 import DashboardScreen from './src/screens/dashboardScreen.jsx';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <ColorDemo /> */}
-      <DashboardScreen />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        {/* <ColorDemo /> */}
+        <Profile />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 };
 
