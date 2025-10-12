39ba27c2ff1b9ca31cf0b669a8e8936ce642b6b4
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-10-12 09:56:57 +0530

    [Feature]:Profile-page.jsx created.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import Profile from './src/screens/Profile.jsx';
 import ColorDemo from './src/screens/ColorDemo.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ColorDemo />
+      {/* <ColorDemo /> */}
+        <Profile />
       <StatusBar style="auto" />
     </View>
   );
