ba1d7da78a5286b3693b2151dc0d884cb02c48c8
Shavidu Gayashan <95381029+Shavindu-Gayashan@users.noreply.github.com>
2025-10-31 00:52:19 +0530

    import screens to app.js

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Profile from './src/screens/Profile.jsx';
+import ColorDemo from './src/screens/ColorDemo.jsx';
 import DashboardScreen from './src/screens/dashboardScreen.jsx';
+import StartScreen from './src/screens/StartScreen.jsx';
+import LoginScreen from './src/screens/LoginScreen.jsx';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
         {/* <ColorDemo /> */}
-        <DashboardScreen />
+        <LoginScreen />
         <StatusBar style="auto" />
       </View>
     </SafeAreaProvider>
